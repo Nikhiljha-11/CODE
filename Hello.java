80ae25ef48fd93287b02a8d70d124069bc72c32a
Nikhil Jha <nikhiljha8825@gmail.com>
2026-01-15 18:54:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello  {
 	public static void main(String args[]){
-		System.out.println("Hello Nikhil");
+		System.out.println("Hello This is Nikhil");
+		System.out.println("I am preparing for the gate exam");
 		
 	}
+
 }
