ac1b8dbed0effa8623907ce1c0760d77cfd9bb58
Nikhil Jha <nikhiljha8825@gmail.com>
2026-02-10 23:15:03 +0530

    Update array in BinarySearch for testing

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,6 @@
 public class BinarySearch{
     public static void main(String[] args){
-        int [] arr = {2,4,6,8,10,12,14,16,18,20};
+        int [] arr = {2,4,6,8,10,15,20};
         int target = 10;
         System.out.println(Search( arr, target, 0 , arr.length-1));
     }
@@ -22,5 +22,6 @@ public class BinarySearch{
 }
 
     
+
 
 
