f16771bc3c8522754ec66a0e0b413d85836ae835
Nikhil Jha <nikhiljha8825@gmail.com>
2026-02-12 17:56:15 +0530

    Update PowerofTwo.java

--- PowerofTwo.java
+++ PowerofTwo.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class PowofTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        boolean ans = (n & (n-1)) ==0;
+        int t = sc.nextInt();
+        boolean ans = (t & (t-1)) ==0;
         System.out.println(ans);
     }
 }
