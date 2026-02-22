c273acc6bb6877515dc02c6ed9f1b11acd05fbf8
Nikhil Jha <nikhiljha8825@gmail.com>
2026-02-22 19:02:13 +0530

    Update Hello.java
    Removed unnecessary lines and simplified the Hello class.

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 public class Hello  {
 	public static void main(String args[]){
 		System.out.println("Hello This is Nikhil");
-		System.out.println("I am preparing for the gate exam");
-		
-	}
+		}
 
 }
+
