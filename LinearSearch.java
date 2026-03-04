e17004a1943ac7a1cbcefe3d85191121c5c27691
Nikhil Jha <nikhiljha8825@gmail.com>
2026-03-04 20:30:13 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -3,7 +3,7 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] arr = {2,4,7,8,9,5,3,1};
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number you want to search: ");
+        System.out.print("Enter the number you want search: ");
         int item = sc.nextInt();
         int temp =0;
         for(int i = 1 ; i<arr.length;i++)
@@ -20,4 +20,5 @@ public class LinearSearch {
         }
     }
 }
+
 
